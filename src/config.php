d1b4077736a9de2288d10262465cd2ca44d169ba
Piffek <patrykpiwko123412@gmail.com>
2017-07-11 20:03:26 +0200

    ref

--- src/config.php
+++ src/config.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'user_id' => '624703367819-p617jf75bj2j9ouqafeqv8dklo1jc4fp.apps.googleusercontent.com',
-    'user_secret' => '4hI5KfBu9AKbYQvT91SSsWkw',
-    'api_key' => 'AIzaSyDPQWAdd2j1Yymfd2AH0G-6JHkvh6GeI8w',
+    'user_id' => '',
+    'user_secret' => '',
+    'api_key' => '',
     'redirect_url' => 'localhost:8000',
     'application_name' => 'My application name',
 ];
